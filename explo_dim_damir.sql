7b7a51fc5959f7dfeec96db44ccaea51031c4bb9
AntoineGiraud <antoine.giraud@outlook.com>
2025-04-25 18:42:27 +0200

    refactor(dim): 2 tables sur les dim damir
    - dim_damir : les colonnes damir
    - dim_damir_cle_libelle : détail des clés par dimension

--- explo_dim_damir.sql
+++ explo_dim_damir.sql
@@ -1,7 +1,11 @@
 
+------------------------------------------------------------------------
+-- Le détail des clés possibles par dimension DAMIR
+------------------------------------------------------------------------
+
 -- préparer les tables de dimension ?
 --> basé sur l'excel lié
--- create table dim_damir as
+create or replace table dim_damir_cle_libelle as
 with brut as (
     select
         A as cle,
@@ -10,7 +14,7 @@ with brut as (
         if( lag(cle) over() is null, 1, 0 ) as rg_mesure,
         row_number() over() rg,
     from read_xlsx('~\Documents\codes\damir_exploration\docs\2024_descriptif-variables_open-damir-base-complete.xlsx',
-                    sheet = 'MOD OPEN DAMIR', range = 'A2:C2400', all_varchar = true)
+                    sheet = 'MOD OPEN DAMIR', range = 'A3:C2400', all_varchar = true, header = false)
     where not ifnull(cle, '') in ('valeur', 'MODALITES VARIABLES OPEN DAMIR')
 ), brut_1 as (
     select
@@ -21,34 +25,59 @@ with brut as (
         rg,
     from brut
     where cle is not null
-), cle_libelle as (
-    select
-        cle::int as cle,
-        libelle,
-        detail,
-        rg_mesure,
-        first_value(cle) over(partition by rg_mesure order by rg) as dimension,
-        replace(first_value(libelle) over(partition by rg_mesure order by rg), 'Libellé', '') as dimension_description,
-    from brut_1
-    qualify cle != dimension -- and detail is not null
-    order by rg_mesure, cle
-    --'MOD OPEN DAMIR'
-    --'OPEN DAMIR'
 )
-select 
-  rg_mesure,
-  dimension,
-  dimension_description,
-  count(1) nb_cle,
-  array_agg( {'cle': cle, 'libelle': libelle} order by cle) valeurs,
-  string_agg(cle || ',' || libelle, '
-' order by cle) as col_csv,
-from cle_libelle
-group by all
-order by 1
+-- cle_libelle
+select
+    first_value(cle) over(partition by rg_mesure order by rg) as dimension,
+    cle::int as cle,
+    libelle,
+    detail,
+    -- rg_mesure,
+    -- replace(first_value(libelle) over(partition by rg_mesure order by rg), 'Libellé', '') as dimension_description,
+from brut_1
+qualify cle != dimension -- and detail is not null
+order by dimension, cle;
 
+-- à quoi ça ressemble ?
+from dim_damir_cle_libelle;
 
--- recap unnested
-select dimension, unnest(valeurs) vals, dimension_description, nb_cle, rg_mesure,
-from dim_damir
+-- recap d'une dim
+select *
+from dim_damir_cle_libelle
 where dimension='PSE_ACT_SNDS'
+
+------------------------------------------------------------------------
+-- Les colonnes & stats
+------------------------------------------------------------------------
+
+-- recap des clé / libellé des axes d'analyse DAMIR
+create or replace table dim_damir as
+with cle_agg as (
+	select 
+	  dimension,
+	  count(1) nb_cle,
+	  array_agg( {'cle': cle, 'libelle': libelle} order by cle)::json[] valeurs,
+	from dim_damir_cle_libelle
+	group by all
+), colonnes as (
+	-- descriptions colonnes
+	SELECT 
+	    C as categorie,
+	    A as dimension,
+	    B as description,
+	    -- D as note,
+	from read_xlsx('~\Documents\codes\damir_exploration\docs\2024_descriptif-variables_open-damir-base-complete.xlsx',
+	                sheet = 'OPEN DAMIR', range = 'A3:D100', all_varchar = true, header = false)
+	where dimension is not null
+)
+select
+	colonnes.*,
+	cle_agg.nb_cle,
+	cle_agg.valeurs,
+from colonnes
+  left join cle_agg using(dimension)
+order by 1,2
+
+
+-- à quoi ça ressemble ?
+from dim_damir
